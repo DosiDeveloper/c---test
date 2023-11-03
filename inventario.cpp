8a46c8bd8d5183ffbd855735fe5efd103f6e88ae
Douglas S Barreto B <dosideveloper@gmail.com>
2023-11-02 22:11:40 -0400

    update 1

--- inventario.cpp
+++ inventario.cpp
@@ -1,18 +1,37 @@
+/**
+ * Este es el archivo que contiene la clase de Inventario
+ *
+ * Contiene _ metodos (procedimientos), que realizan las siguientes funciones
+ *
+ *
+ */
+
 #include <fstream>
 #include <iostream>
 #include <cstdlib>
 #include <iomanip>
 #include <vector>
-#include <typeinfo>
+#include <sstream>
 
+using std::cin;
+using std::cout;
+using std::endl;
 using std::string;
 using std::vector;
 
 struct camposInv
 {
-    int idn;
-    string marca;
-    string nota;
+    int NBP;
+    string MARCA;
+    string DEPTO;
+    string USUARIO;
+    string FECHAREGISTRO;
+    string FECHAMOD;
+    string FECHADESIN;
+    string toStringFormat()
+    {
+        return NBP + "|" + MARCA + "|" + DEPTO + "|" + USUARIO + "|" + FECHAREGISTRO + "|" + FECHAMOD + "|" + FECHADESIN;
+    }
 };
 
 class Inventario
@@ -20,89 +39,98 @@ class Inventario
 private:
     string nombreArchivo;
     std::ofstream archivoActivo;
-    vector<camposInv> registros;
+    camposInv campInv;
+    time_t tiempo = time(0);
+    struct tm *tlocal = localtime(&tiempo);
+    std::ostringstream tiempo_b;
+
 
 public:
     Inventario(string NombreArchivo)
     {
-        std::cout.setf(std::ios::left, std::ios::right);
-        std::cout.width(50);
+        system("cls");
+        cout.setf(std::ios::adjustfield);
+        cout.width(50);
         this->nombreArchivo = NombreArchivo;
-        std::cout << std::endl;
-        std::cout << std::setfill('*') << "Bienvenido al Sistema de Inventario" << std::setfill('*') << std::endl;
-        std::cout << std::endl;
+        cout << "Bienvenido al Sistema de Inventario" << endl;
     }
 
-    insertarDataArchivo(string Data)
+    void insertarDataArchivo(string Data)
     {
 
         if (this->archivoActivo.fail())
         {
-            std::cout << "No se pudo abrir el archivo";
+            cout << "No se pudo abrir el archivo";
             exit(1);
         }
 
         if (this->archivoActivo.is_open())
         {
-            this->archivoActivo.write(Data.c_str(), sizeof Data);
+            this->archivoActivo.write(Data.c_str(), sizeof Data.c_str());
         }
         else
         {
             this->archivoActivo.open(this->nombreArchivo);
-            this->archivoActivo.write(Data.c_str(), sizeof Data);
+            this->archivoActivo.write(Data.c_str(), sizeof Data.c_str());
         }
+        return;
     }
 
-    insertarNuevoRegistro()
+    void insertarNuevoRegistro()
     {
+        system("cls");
         while (true)
         {
-            int idn;
-            string marca, nota;
-            std::cout << "Ingrese los siguientes datos: " << std::endl;
-            std::cout << "- Identificador: ";
-            std::cin >> idn;
-            std::cout << "- Marca: ";
-            std::cin >> marca;
-            std::cout << "- Nota: ";
-            std::cin >> nota;
-            std::cout << "Estos son los datos ingresados: " << format << std::endl;
-            std::cout << "Desea guardar e ingresar otro registro? (y / n): ";
+            cout << "Ingrese los siguientes datos: " << endl;
+            cout << "- Identificador: ";
+            cin >> this->campInv.NBP;
+            cout << "- Marca: ";
+            cin >> this->campInv.MARCA;
+            cout << "- Departamento: ";
+            cin >> this->campInv.DEPTO;
+            this->tiempo_b << std::put_time(&this->tlocal, "%d/%m/%Y");
+            this->campInv.FECHAREGISTRO = this->tiempo_b.str();
+            this->campInv.FECHAMOD = "00/00/0000";
+            this->campInv.FECHADESIN = "00/00/0000";
+            
+            cout
+                << "Estos son los datos ingresados: " << endl;
+
+
+            cout << "Desea guardar e ingresar otro registro? (y / n): ";
             char select;
-            std::cin >> select;
+            cin >> select;
             if (select == 'y' || select == 'Y')
             {
-                this->registros.push_back(camposInv{
-                    idn,
-                    marca,
-                    nota});
+                this->insertarDataArchivo(this->campInv.toStringFormat());
             }
             else
             {
                 break;
             }
         }
+        return;
     }
 
-    menu()
+    void menu()
     {
-        while (true)
+        do
         {
-
+            system("cls");
             int select;
-            std::cin.setf(std::ios::right);
-            std::cout << std::left << "Elija una de las acciones que desee realizar en el sistema de inventario"
-                      << std::endl;
-            std::cout << std::left << "Crear un nuevo registro en el inventario ( 1 )"
-                      << std::endl;
-            std::cout << std::left << "Eliminar un registro existente en el inventario ( 2 )"
-                      << std::endl;
-            std::cout << std::left << "Editar un registro existente en el inventario ( 3 )"
-                      << std::endl;
-            std::cout << std::left << "Salir del programa ( 4 )"
-                      << std::endl;
-            std::cout << std::right << "Su seleccion: ";
-            std::cin >> select;
+            cin.setf(std::ios::right);
+            cout << std::left << "Elija una de las acciones que desee realizar en el sistema de inventario"
+                 << endl;
+            cout << std::left << "- Crear un nuevo registro en el inventario ( 1 )"
+                 << endl;
+            cout << std::left << "- Eliminar un registro existente en el inventario ( 2 )"
+                 << endl;
+            cout << std::left << "- Editar un registro existente en el inventario ( 3 )"
+                 << endl;
+            cout << std::left << "- Salir del programa, cualquier otro numero para salir"
+                 << endl;
+            cout << std::right << "* Su seleccion: ";
+            cin >> select;
 
             switch (select)
             {
@@ -117,7 +145,9 @@ public:
 
             default:
                 std::cout << "Seleccione una opcion valida";
+                break;
             }
-        }
+        } while (true);
+        return;
     }
 };
--- main.cpp
+++ main.cpp
@@ -1,3 +1,8 @@
+/**
+ * Este es el archivo principal del sistema de inventario
+ * Author: Douglas Barreto
+ * Seccion: FL-1410612+
+*/
 #include <iostream>
 #include "inventario.cpp"
 
